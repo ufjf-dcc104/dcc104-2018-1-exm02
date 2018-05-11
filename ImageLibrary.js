0a3f68e696fb55268874ba1b032517c1dcb577e7
ICE <ice@lab05m51.nrc.ice.ufjf.br>
2018-05-11 20:31:10 -0300

    Adciona desenho com angulo

--- ImageLibrary.js
+++ ImageLibrary.js
@@ -15,3 +15,13 @@ ImageLibrary.prototype.draw = function (ctx, key, x, y) {
 ImageLibrary.prototype.drawSize = function (ctx, key, x, y, w, h) {
   ctx.drawImage(this.images[key], x, y, w, h);
 };
+
+ImageLibrary.prototype.drawAngle = function(ctx, key, x, y, ang){
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate(ang*Math.PI/180);
+  //ctx.fillStyle = "white";
+  //ctx.fillRect(-this.images[key].width/2,-this.images[key].width/2,this.images[key].width,this.images[key].height);
+  ctx.drawImage(this.images[key], -this.images[key].width/2, -this.images[key].height/2);
+  ctx.restore();
+}
