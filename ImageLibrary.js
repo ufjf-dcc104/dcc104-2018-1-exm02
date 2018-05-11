bd1de31361b64e00f550c873b385c3356e5ae0b8
ICE <ice@lab05m51.nrc.ice.ufjf.br>
2018-05-11 19:52:46 -0300

    Adiciona carregamento e desenho

--- ImageLibrary.js
+++ ImageLibrary.js
@@ -1,3 +1,13 @@
 function ImageLibrary(){
   this.images = {};
 }
+
+ImageLibrary.prototype.load = function (key,url) {
+  var img = new Image();
+  this.images[key] = img;
+  img.src = url;
+};
+
+ImageLibrary.prototype.draw = function (ctx, key, x, y) {
+  ctx.drawImage(this.images[key], x, y);
+};
