30aeae97867e950de0698fa6c76a9d646e320051
Igor Knop <igorknop@gmail.com>
2018-05-18 19:58:13 -0300

    Adiciona play em canais

--- AudioLibrary.js
+++ AudioLibrary.js
@@ -1,10 +1,13 @@
 AudioLibrary = function(){
-  this.MAX_CANAIS = 5;
+  this.MAX_CANAIS = 15;
   this.audios = {};
   this.canais = [];
 
   for (var i = 0; i < this.MAX_CANAIS; i++) {
-    this.canais[i] = new Audio();
+    this.canais[i] = {
+      audio: new Audio(),
+      fim: -1
+    }
   }
 };
 
@@ -15,5 +18,14 @@ AudioLibrary.prototype.load = function (key, url) {
 };
 
 AudioLibrary.prototype.play = function (key) {
-  this.audios[key].play();
+  var agora = new Date();
+  for (var i = 0; i < this.canais.length; i++) {
+    var canal = this.canais[i];
+    if(canal.fim < agora.getTime()){
+      canal.audio.src = this.audios[key].src;
+      canal.fim = this.audios[key].duration*1000+agora.getTime();
+      canal.audio.play();
+      break;
+    }
+  }
 };
